7e33768bde49614839074e214c4cf3dab386fcdc
Martin9 <martinjcantillo9@gmail.com>
2025-05-01 19:06:55 -0500

    deleting project

--- Front-Studyhub/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { LayoutComponent } from './app/layout/layout.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-bootstrapApplication(LayoutComponent, {
-  providers: [
-    provideRouter(routes),
-  ]
-});
